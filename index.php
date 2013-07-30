0b261f193d62e4b4437c72e98f41a847eecd2cfd
Ric Allinson <capecodehq@yahoo.co.uk>
2013-07-30 12:26:27 -0700

    Updates to make this work in the real world.

--- index.php
+++ index.php
@@ -17,15 +17,40 @@ class KeyVal {
         }
     }
 
+    public function genUuid() {
+
+        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_mid"
+            mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand( 0, 0x0fff ) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand( 0, 0x3fff ) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
+    }
+
     public function put($key, $val) {
 
         $fullkey = $this->prefix . $key;
 
-        file_put_contents($fullkey, $val);
+        $bytes = file_put_contents($fullkey, json_encode($val));
 
-        if ($this->ttl) {
+        if ($bytes > 0 && $this->ttl) {
             apc_store($fullkey, $val, $this->ttl);
         }
+
+        return $bytes > 0 ? true : false;
     }
 
     public function get($key) {
@@ -44,7 +69,7 @@ class KeyVal {
             return null;
         }
 
-        $val = file_get_contents($fullkey);
+        $val = json_decode(file_get_contents($fullkey), true);
 
         if ($this->ttl) {
             $this->put($key, $val);
@@ -61,7 +86,24 @@ class KeyVal {
             apc_delete($fullkey);
         }
 
-        unlink($fullkey);
+        return unlink($fullkey);
+    }
+
+    public function getKeys($from=0, $to=null) {
+
+        $keys = array();
+        $dh  = opendir($this->prefix);
+
+        while (false !== ($filename = readdir($dh))) {
+            if (is_file($this->prefix . $filename)) {
+                $keys[] = $filename;
+            }
+            if ($to && --$to < $from) {
+                return $keys;
+            }
+        }
+
+        return $keys;
     }
 }
 
